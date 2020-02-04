255c1ff7fb1d2a08cfb06e06392c55eea59f6781
Nathan Johnson <nathanjohnson@Nathans-MBP.lan>
2020-02-04 08:49:57 -0600

    launched-version

--- njt-client/src/components/footer.js
+++ njt-client/src/components/footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  makeStyles
+} from '@material-ui/core/styles';
 import {
   Link
 } from "react-router-dom";
@@ -16,7 +18,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 import Icons from './icons'
 
 //CSS
-import'../App.css';
+import '../App.css';
 
 
 
@@ -35,69 +37,69 @@ const useStyles = makeStyles(theme => ({
     fontSize: '3rem',
   },
   Toolbar: {
-      maxWidth: 1400,
-      width: '100%',
-      margin: 'auto'
+    maxWidth: 1400,
+    width: '100%',
+    margin: 'auto'
   },
 
-icons: {
+  icons: {
     fontSize: '3rem',
     color: '#959595',
     '&:hover': {
-        color: '#FFFFFF',
+      color: '#FFFFFF',
     },
-},
-footer: {
-  width: '100%',
-  paddingTop: 10,
-  paddingBottom: 15,
-  display: 'flex',
-  height: 180,
-  overflow: 'hidden'
-},
-centered1: {
-  margin: 'auto',
-  height: 150,
-},
-
-footerImage:{
-  height: 180,
-  "&:hover": {
-    cursor: 'pointer',
-    transform : 'scale(1.05)'
-  }
-},
-
-right: {
-position: 'absolute',
-width:'45%',
-right: 0,
-top: '50%',
-transform: 'translate(-0%, -50%)',
-overflow: 'hidden',
-[theme.breakpoints.down('xs')]: {
-  fontSize: '.7rem',
-  width:'20%',
-  marginRight: 12
-},
-},
-
-left: {
-  position: 'absolute',
-  width:'45%',
-  left: 0,
-  textAlign: 'right',
-  top: '50%',
-  transform: 'translate(-0%, -50%)',
-  fontFamily: 'Helvetica Neue',
-  fontSize: '1rem',
-  color: '#707070',
-  [theme.breakpoints.down('xs')]: {
-    fontSize: '.7rem',
-    width:'30%',
   },
+  footer: {
+    width: '100%',
+    paddingTop: 10,
+    paddingBottom: 15,
+    display: 'flex',
+    height: 180,
+    overflow: 'hidden'
   },
-  njt:{
+  centered1: {
+    margin: 'auto',
+    height: 150,
+  },
+
+  footerImage: {
+    height: 180,
+    "&:hover": {
+      cursor: 'pointer',
+      transform: 'scale(1.05)'
+    }
+  },
+
+  right: {
+    position: 'absolute',
+    width: '45%',
+    right: 0,
+    top: '50%',
+    transform: 'translate(-0%, -50%)',
+    overflow: 'hidden',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '.7rem',
+      width: '20%',
+      marginRight: 12
+    },
+  },
+
+  left: {
+    position: 'absolute',
+    width: '45%',
+    left: 0,
+    textAlign: 'right',
+    top: '50%',
+    transform: 'translate(-0%, -50%)',
+    fontFamily: 'Helvetica Neue',
+    fontSize: '1rem',
+    color: '#707070',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '.7rem',
+      width: '30%',
+    },
+  },
+  njt: {
     fontFamily: 'FunCity',
     paddingRight: 20,
     color: 'white'
@@ -109,35 +111,68 @@ left: {
 export default function Navbar() {
   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar  className={classes.Toolbar} centered='True' >
-        
-    
-        <div className={classes.footer}>  
+  return ( <
+    div className = {
+      classes.root
+    } >
+    <
+    AppBar position = "static" >
+    <
+    Toolbar className = {
+      classes.Toolbar
+    }
+    centered = 'True' >
 
-        <div className={classes.centered1}>  
 
-        <Link to="/contact">
-     
-        <Tooltip title="Contact Page">
-          <img src={faceIcon} className={classes.footerImage} alt="NJT"/>
-</Tooltip>
-</Link>
-          </div>       
-          <div className={classes.right}>  
-          <Icons/>
-          </div>
+    <
+    div className = {
+      classes.footer
+    } >
 
-          <div className={classes.left}>  
-          <h1>CONTACT
-          <div className={classes.njt}> NJT </div>
-          </h1>
-          </div>
-          </div>  
-        </Toolbar>
-      </AppBar>
-    </div>
+    <
+    div className = {
+      classes.centered1
+    } >
+
+    <
+    Link to = "/contact" >
+
+    <
+    Tooltip title = "Contact Page" >
+    <
+    img src = {
+      faceIcon
+    }
+    className = {
+      classes.footerImage
+    }
+    alt = "NJT" / >
+    <
+    /Tooltip> <
+    /Link> <
+    /div>        <
+    div className = {
+      classes.right
+    } >
+    <
+    Icons / >
+    <
+    /div>
+
+    <
+    div className = {
+      classes.left
+    } >
+    <
+    h1 > CONTACT <
+    div className = {
+      classes.njt
+    } > NJT < /div> <
+    /h1> <
+    /div> <
+    /div>   <
+    /Toolbar> <
+    /AppBar> <
+    /div>
   );
 }
--- njt-client/src/pages/home.js
+++ njt-client/src/pages/home.js
@@ -1,199 +1,197 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
-import {
-    Link
-  } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // jpeg icons
-import handshake from '../images/Icons/handshake.jpg'
-import recycle from '../images/Icons/recycleicon.jpg'
-import plcIcon from '../images/Icons/plcIcon.jpg'
+import handshake from '../images/Icons/handshake.jpg';
+import recycle from '../images/Icons/recycleicon.jpg';
+import plcIcon from '../images/Icons/plcIcon.jpg';
 
 //Images
-import parts from '../images/parts.jpg'
-import worker from '../images/worker.jpg'
-import machines from '../images/machines.jpg'
-import globe from '../images/globe.jpg'
+import parts from '../images/parts.jpg';
+import worker from '../images/worker.jpg';
+import machines from '../images/machines.jpg';
+import globe from '../images/globe.jpg';
 
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
 
 //CSS
-import'../App.css';
+import '../App.css';
 
 class home extends Component {
-    render() {
+	render() {
+		return (
+			<div>
+				<Helmet>
+					<meta
+						property='og:image'
+						content='https://njtautomation.com/static/media/parts.84e59de7.jpg'
+					/>
+					<meta
+						id='og-image'
+						property='og:image:secure_url'
+						content='https://njtautomation.com/static/media/parts.84e59de7.jpg'
+					/>
+					<link rel='icon' href='/njt-client/public/NJT.png' />
+					<meta name='viewport' content='width=device-width,initial-scale=1' />
+					<meta name='theme-color' content='#000000' />
+					<meta
+						name='description'
+						content='Sell Surplus and Used Automation Equipment - Milwaukee, WI'
+					/>
+					<link
+						rel='apple-touch-icon'
+						href='/njt-client/src/images/parts.jpg'
+					/>
+					<link rel='manifest' href='/manifest.json' />
+					<title>NJT Automation Salvage</title>
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1'></meta>
+					<meta NAME='geo.position' CONTENT='43.0389; 87.9065'></meta>
+					<meta NAME='geo.placename' CONTENT='Milwaukee'></meta>
+				</Helmet>
+				<div className='header'>
+					<h1> Keeping The Machines Alive </h1>
+					<h2> An entire operation can hinge on one part </h2>
+				</div>
+				<div className='section'>
+					<div className='section1'>
+						<Grid
+							container
+							spacing={2}
+							direction='row'
+							justify='center'
+							alignItems='center'>
+							<Grid item sm={3} xs={12}>
+								<div className='firstchild'>
+									<h1>
+										Services
+										<div> For&nbsp;Your </div>
+										Business
+									</h1>
+								</div>
+							</Grid>
+							<Grid
+								item
+								sm={3}
+								xs={12}
+								component={Link}
+								to='/sell'
+								style={{ textDecoration: 'none' }}>
+								<div className='block'>
+									<img src={handshake} alt='Sell Electronic E Waste'></img>
+									<div>Sell Surplus Automation Components</div>
+								</div>
+							</Grid>
+							<Grid
+								item
+								sm={3}
+								xs={12}
+								component={Link}
+								to='/E-waste'
+								style={{ textDecoration: 'none' }}>
+								<div className='block'>
+									<img
+										src={recycle}
+										alt='Recycle Electronic E Waste Milwaukee'></img>
+									<div>
+										E-Waste
+										<section>Removal and Recycling</section>
+									</div>
+								</div>
+							</Grid>
+							<Grid
+								item
+								sm={3}
+								xs={12}
+								component={Link}
+								to='/sourcing'
+								style={{ textDecoration: 'none' }}>
+								<div className='block'>
+									<img
+										src={plcIcon}
+										alt='Recycle Electronic PLC  Milwaukee'></img>
+									<div>
+										Rare
+										<section>Parts</section>
+										Sourcing
+									</div>
+								</div>
+							</Grid>
+						</Grid>
+					</div>
+				</div>
 
-        return (
-            
-            <div>
-            <Helmet>
-            <meta property="og:image" content="https://njtautomation.com/static/media/parts.84e59de7.jpg"/>
-            <meta id="og-image" property="og:image:secure_url" content="https://njtautomation.com/static/media/parts.84e59de7.jpg"/>
-            <link rel="icon" href="/njt-client/public/NJT.png"/>
-            <meta name="viewport" content="width=device-width,initial-scale=1"/>
-            <meta name="theme-color" content="#000000"/>
-            <meta name="description" content="Sell Surplus and Used Automation Equipment - Milwaukee, WI"/>
-            <link rel="apple-touch-icon" href="/njt-client/src/images/parts.jpg"/><link rel="manifest" href="/manifest.json"/>
-            <title>NJT Automation Salvage</title>
-            <meta name='viewport' content="width=device-width, initial-scale=1"></meta>
-          <meta NAME="geo.position" CONTENT="43.0389; 87.9065"></meta>
-            <meta NAME="geo.placename" CONTENT="Milwaukee"></meta> 
-            </Helmet>
-            <div className="header">
-            <h1> Keeping The Machines Alive </h1>
-            <h2> An entire operation can hinge on one part </h2>
-            </div>
-            <div className="section">
-            <div className="section1">
-            <Grid container spacing={2}
-            direction="row"
-            justify="center"
-            alignItems="center">
-            <Grid item sm={3} xs={12}>
+				<div className='body'>
+					<Link to='/contact' style={{ textDecoration: 'none' }}>
+						<div className='wanted' component={Link} to='/contact'>
+							<h1>AUTOMATION COMPONENTS WANTED</h1>
+						</div>
+					</Link>
+					<Grid container spacing={2} direction='row'>
+						<Grid item sm={6} xs={12}>
+							<div className='parts'>
+								<h1>
+									Programmable Logic Controllers (PLC)
+									<section>Servo Controllers</section>
+									<section>Panel View Screens</section>
+									<section>HMI and Touch Screens</section>
+									<section>Variable Frequency Drives (VFD)</section>
+									and similar industrial controls
+								</h1>
+								<h2>
+									Brands: Rockwell Automation, Allen Bradley, Siemens, Lenze,
+									ABB, Banner, Schneider Electric, Omron, Mitsubishi, GE,
+									Rockwell, Bosch, Danfoss, Baldor, Fuji, Reliant Electric
+								</h2>
+							</div>
+						</Grid>
+						<Grid item sm={12} xs={12}>
+							<img
+								className='partsImage'
+								src={parts}
+								alt='Automation Parts'></img>
+						</Grid>
+					</Grid>
+					<div className='images'>
+						<div className='worker'>
+							<h1>
+								Saving old machines
+								<div>saves jobs</div>
+								and our planet
+							</h1>
+							<img src={worker} alt='Manufacturing Factory Worker'></img>
+						</div>
+						<div className='machines'>
+							<h1>
+								NJT
+								<a href='sms:+1-414-331-5954'>
+									<div className='phone'> (414)331-5954</div>
+								</a>
+							</h1>
 
-            <div className="firstchild">
-            <h1>
-            Services 
-            <div> For&nbsp;Your </div>
-             Business
-            </h1>  
-            </div>
-
-            </Grid> 
-            <Grid item sm={3} xs={12} component={Link} to="/sell" style={{ textDecoration: 'none' }}>
-                <div className="block">
-                <img src={handshake} alt="Sell Electronic E Waste">
-                </img>
-                <div>
-                Sell Surplus
-                Automation
-                Components
-                </div>
-                </div>
-            </Grid>
-            <Grid item sm={3} xs={12} component={Link} to="/E-waste" style={{ textDecoration: 'none' }}>
-                <div className="block">
-                <img src={recycle} alt="Recycle Electronic E Waste Milwaukee">
-                </img>
-                <div>
-                E-Waste
-                <section>
-                Removal and
-                Recycling
-                </section>
-                </div>
-            </div>
-            </Grid>
-                <Grid item sm={3} xs={12} component={Link} to="/sourcing" style={{ textDecoration: 'none' }}>
-                 <div className="block">
-                 <img src={plcIcon} alt="Recycle Electronic PLC  Milwaukee">
-                 </img>
-                 <div>
-                Rare
-                <section>
-                Parts
-                </section>
-                Sourcing
-                </div>
-            </div>
-            </Grid>
-          </Grid>
-            </div>
-            </div>
-
-
-            <div className="body">
-            <Link to="/contact"  style={{ textDecoration: 'none' }} >
-            <div className="wanted" component={Link} to="/contact">
-            <h1>AUTOMATION COMPONENTS WANTED</h1>
-            </div>
-            </Link>
-            <Grid container spacing={2}
-                direction="row"
-                >
-                <Grid item sm={6} xs={12}>
-            <div className="parts">
-            <h1>
-            Programmable Logic Controllers (PLC)
-            <section>
-            Servo Controllers
-            </section>
-            <section>
-            Panel View Screens
-            </section>
-            <section>
-            HMI and Touch Screens            
-            </section>
-            <section>
-            Variable Frequency Drives (VFD)
-            </section>
-            and similar industrial controls
-            </h1>
-            <h2>
-            Brands:
-            Rockwell Automation, Allen Bradley, Siemens, Lenze, 
-            ABB, Banner, Schneider Electric, Omron, 
-            Mitsubishi, GE, Rockwell, 
-            Bosch, Danfoss, Baldor,
-            Fuji, Reliant Electric
-            </h2>
-            </div>
-            </Grid>
-            <Grid item sm={12} xs={12}
-            >
-            <img className="partsImage" src={parts} alt="Automation Parts">
-            </img>
-            </Grid>
-            </Grid>
-            <div className='images'>
-          
-            <div className="worker">
-            <h1>
-            Saving old machines
-            <div>saves jobs</div>
-            and our planet</h1>
-            <img src={worker} alt="Manufacturing Factory Worker">
-            </img>
-            </div>
-            <div className="machines">
-            <h1>
-            NJT
-            <a href="sms:+1-414-331-5954">
-            <div className="phone" > (414)331-5954</div>
-            </a>
-            </h1>
-            
-            
-            <img className="machines" src={machines} alt="Manufacturing Machines">
-            </img>
-            </div>
-            <div className="globe">
-            <h1>
-            Every corner of the globe
-            <div>
-            has automated machinery,
-            </div>
-            <div>
-            commonly 20+ years old.
-
-            </div>
-            <div>
-            It is far too costly to upgrade.
-            </div>
-            The old machines 
-            <div>
-            must 
-            stay operational.
-            </div>
-            </h1>
-            <img src={globe} alt="Global E commerce in manufacturing">
-            </img>
-            </div>
-            </div>
-            </div>
-            </div>
-        )
-    }
+							<img
+								className='machines'
+								src={machines}
+								alt='Manufacturing Machines'></img>
+						</div>
+						<div className='globe'>
+							<h1>
+								Every corner of the globe
+								<div>has automated machinery,</div>
+								<div>commonly 20+ years old.</div>
+								<div>It is far too costly to upgrade.</div>
+								The old machines
+								<div>must stay operational.</div>
+							</h1>
+							<img src={globe} alt='Global E commerce in manufacturing'></img>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default home;
